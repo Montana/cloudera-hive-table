079c2d4471668a1ec2f7b6a845baa5aa1431be65
Montana Mendy <montana@linux.com>
2022-05-13 18:19:59 -0700

    push scala test via vim

---
+++ Main.scala
@@ -0,0 +1,15 @@
+import org.apache.hadoop.fs.Path
+import java.io.PrintWriter
+
+object Main extends App with Config {
+  println("Trying to write to HDFS...")
+  val output = fs.create(new Path("/user/root/Mr_shah"))
+  val writer = new PrintWriter(output)
+  try {
+    writer.write("this is a test")
+    writer.write("\n")
+  } finally {
+    writer.close()
+  }
+  print("Done!")
+}
